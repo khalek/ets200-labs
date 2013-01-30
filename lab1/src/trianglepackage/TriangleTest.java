2b203f8790248c6427e215fc3b84822f748308eb
khalek <fredrik.stil@gmail.com>
2013-01-30 18:47:03 +0100

    Added a test for equilateral

--- lab1/src/trianglepackage/TriangleTest.java
+++ lab1/src/trianglepackage/TriangleTest.java
@@ -9,15 +9,13 @@ import junit.framework.TestSuite;
  * Students should add relevant unit test cases related to the Triangle 
  * class to this class.
  */
-public class TriangleTest extends TestCase
-{
+public class TriangleTest extends TestCase {
 	private Triangle rightAngledTriangle;
 
 	/**
 	 * Constructs a test case with the given name.
 	 */
-	public TriangleTest(String name)
-	{
+	public TriangleTest(String name) {
 		super(name);
 	}
 
@@ -25,8 +23,7 @@ public class TriangleTest extends TestCase
 	 * Initializes common objects. The JUnit framework automatically invokes 
 	 * this method before each test is run.
 	 */
-	protected void setUp()
-	{
+	protected void setUp() {
 		rightAngledTriangle = new Triangle(3, 4, 5);
 	}
 
@@ -34,21 +31,28 @@ public class TriangleTest extends TestCase
 	 * Cleanup method. The JUnit framework automatically invokes
 	 * this method after each test is run.
 	 */
-	protected void tearDown()
-	{
+	protected void tearDown() {
 		rightAngledTriangle = null;
+	}
+	
+	public void testEquilateral() {
+		Triangle equilateral = rightAngledTriangle;
+		equilateral.setSideLengths(1, 1, 1);
+		
+		assertTrue("Should return true for a equilateral triangle",
+					equilateral.isEquilateral());
+		assertEquals("Should return 'equilateral'", "equilateral", equilateral.classify());
 	}
 	
 	/**
 	 * Test whether the triangle specified in the fixture (setUp) 
 	 * is right-angled. 	
 	 */
-	public void testRightAngled()
-	{
+	public void testRightAngled() {
 		assertTrue("Should return true for a right-angled triangle",
 		            rightAngledTriangle.isRightAngled());
 		assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());			
-	}
+	}	
 
 	/**
 	 * Creates a test suite. You can use this if you have a hierarchy of 
@@ -56,8 +60,7 @@ public class TriangleTest extends TestCase
 	 * suite.addTest(TriangleTest.suite())
 	 * @return a test suite 
 	 */
-	public static Test suite()
-	{
+	public static Test suite() {
 		TestSuite suite = new TestSuite(TriangleTest.class);
 		return suite;
 	}
@@ -66,8 +69,7 @@ public class TriangleTest extends TestCase
 	 * Main function for starting the TestRunner.
 	 * @param args no parameters required.
 	 */
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		String[] testCaseName = { TriangleTest.class.getName()};
 
 		// chooses the userinterface
